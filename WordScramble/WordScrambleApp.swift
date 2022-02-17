90cf00b1f02f22d64d03ce8b973ab4f242959cd2
Jordan Haynes <Jordancautious@gmail.com>
2022-02-17 11:32:59 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jordan Haynes on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
